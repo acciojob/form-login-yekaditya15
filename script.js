fead6c6783e2b65cc4fc4e51a1d12f49f360163b
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-11-28 13:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from refreshing the page
+    event.preventDefault();
 
+    // Get the form element by its ID
+    const form = document.getElementById("form1");
+
+    // Access the values of the input fields
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+
+    // Display the full name using an alert
+    alert(`${firstName} ${lastName}`);
 }
